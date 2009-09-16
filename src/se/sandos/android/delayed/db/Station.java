fce3825ca4c68ff8660fc9396c1d748d4d088ce9
John Backstrand <sopues@gmail.com>
2009-09-16 20:26:04 +0200

    Try to send list to new activity

--- src/se/sandos/android/delayed/db/Station.java
+++ src/se/sandos/android/delayed/db/Station.java
@@ -1,6 +1,9 @@
 package se.sandos.android.delayed.db;
 
-public class Station {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Station implements Parcelable {
 	private String mName;
 	private String mUrl;
 	
@@ -17,4 +20,25 @@ public class Station {
 	public String getmUrl() {
 		return mUrl;
 	}
+
+	public int describeContents() {
+		return 0;
+	}
+
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(mName);
+		dest.writeString(mUrl);
+	}
+	
+	public static Parcelable.Creator creator = new Parcelable.Creator(){
+
+		public Object createFromParcel(Parcel source) {
+			return new Station(source.readString(), source.readString());
+		}
+
+		public Object[] newArray(int size) {
+			return new Station[size];
+		}
+		
+	};
 }

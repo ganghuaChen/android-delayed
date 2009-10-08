b5840fdc9d44d2dc0872bf6ee9e818ba06cb17e8
John Backstrand <sopues@gmail.com>
2009-10-08 08:58:50 +0200

    Finish moving stationscraping to proper scraper implementation

--- src/se/sandos/android/delayed/StationActivity.java
+++ src/se/sandos/android/delayed/StationActivity.java
@@ -1,20 +1,13 @@
 package se.sandos.android.delayed;	
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.StringEscapeUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import se.sandos.android.delayed.db.Station;
+import se.sandos.android.delayed.scrape.ScrapeListener;
+import se.sandos.android.delayed.scrape.ScraperHelper;
+import se.sandos.android.delayed.scrape.StationScraper;
 import se.sandos.android.delayed.scrape.ScraperHelper.Nameurl;
 import android.app.ListActivity;
 import android.content.Intent;
@@ -32,8 +25,17 @@ public class StationActivity extends ListActivity {
 	
 	private Handler mHandler = new Handler() {
 		@SuppressWarnings("unchecked")
-		public void handleMessage(Message msg) {
-			if(msg.what == TrainEvent.MSG_DEST) {
+		public void handleMessage(final Message msg) {
+			runOnUiThread(new Runnable(){
+				public void run() {
+					handle(msg);
+				}
+			});
+		}
+
+		private void handle(Message msg) {
+			Log.v(Tag, "Got message: " + msg.what);
+			if(msg.what == StationScraper.MSG_DEST) {
 				
 				//Got a proper destination for some particular train. Mend.
 				Object[] vals = (Object[]) msg.obj;
@@ -56,22 +58,21 @@ public class StationActivity extends ListActivity {
 				return;
 			}
 			
-			List<TrainEvent> l = (List<TrainEvent>) msg.obj;
+			TrainEvent te  = (TrainEvent) msg.obj;
 			
 			if(listContent == null) {
 				listContent = new ArrayList<Map<String, String>>();
 			}
 			
-			for(TrainEvent te : l) {
-				Map<String, String> m = new HashMap<String, String>();
-				Log.i(Tag, "Adding " + te.toString());
-				m.put("name", te.toString());
-				m.put("track", "Track: " + te.getTrack());
-				m.put("number", "  Train #: " + Integer.toString(te.getNumber()));
-				m.put("destination", te.getDestination());
-				m.put("url", te.getUrl());
-				listContent.add(m);
-			}
+			Map<String, String> m = new HashMap<String, String>();
+			Log.i(Tag, "Adding " + te.toString());
+			m.put("name", te.toString());
+			m.put("track", "Track: " + te.getTrack());
+			m.put("number", "  Train #: " + Integer.toString(te.getNumber()));
+			m.put("destination", te.getDestination());
+			m.put("url", te.getUrl());
+			Log.v(Tag, "Te: " + m);
+			listContent.add(m);
 			
 			if(sa == null) {
 				sa = new SimpleAdapter(getApplicationContext(), listContent, R.layout.traineventrow, 
@@ -96,43 +97,21 @@ public class StationActivity extends ListActivity {
 		final String url = i.getStringExtra("url");
 		final String name = i.getStringExtra("name");
 
-		new Thread(){
-			public void run(){
-				try {
-					HttpClient hc = new DefaultHttpClient();
-					
-					String base = "http://m.banverket.se/";
-					String stationSearch = base + url;
-					Log.i(Tag, "Full url: " + stationSearch);
-					HttpGet hg = new HttpGet(stationSearch);
-			
-					Log.i(Tag, "fetching http");
-					HttpResponse hr = hc.execute(hg);
-					Log.i(Tag, "Got page: " + hr.getStatusLine());
-					InputStream is = hr.getEntity().getContent();
-					InputStreamReader isr = new InputStreamReader(is);
-					BufferedReader br = new BufferedReader(isr);
-					String s = null;
-					List<TrainEvent> events = new ArrayList<TrainEvent>(20);
-					//XXX: Move this out to scraper
-					TrainEvent te = new TrainEvent(new Station(name, url));
-					while ((s = br.readLine()) != null) {
-						String unescaped = StringEscapeUtils.unescapeHtml(s);
-						te.parse(unescaped, mHandler);
-						if(te.isParsed()) {
-							events.add(te);
-							
-							List<TrainEvent> add = new ArrayList<TrainEvent>(1);
-							add.add(te);
-							mHandler.sendMessage(Message.obtain(mHandler, 0, add));
-							Log.i(Tag, "Sending trainevent: " + te);
-							te = new TrainEvent();
-						}
-					}
-				} catch(Throwable e) {
-					Log.w(Tag, "Error when fetching: " + e.getMessage());
-				}
+		ScraperHelper.scrapeStation(url, name, new ScrapeListener<TrainEvent, Object>(){
+
+			public void onFinished(Object result) {
+				// TODO Auto-generated method stub
+				
 			}
-		}.start();
+
+			public void onPartialResult(TrainEvent result) {
+				mHandler.dispatchMessage(Message.obtain(mHandler, 0, result));
+			}
+
+			public void onRestart() {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 }
--- src/se/sandos/android/delayed/TrainEvent.java
+++ src/se/sandos/android/delayed/TrainEvent.java
@@ -1,18 +1,10 @@
 package se.sandos.android.delayed;
 
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 import se.sandos.android.delayed.db.Station;
-import se.sandos.android.delayed.scrape.ScraperHelper;
-import se.sandos.android.delayed.scrape.ScraperHelper.Job;
-import se.sandos.android.delayed.scrape.ScraperHelper.Nameurl;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
 
 /**
  * Arrival/departure from one particular station
@@ -22,15 +14,13 @@ import android.util.Log;
 public class TrainEvent {
 	private final static String Tag = "TrainEvent";
 	
-	public final static int MSG_DEST = 1;
-	
 	private Station destination;
 	private Station station;
 	private int id = -1;
 	private String track;
 	private Date arrival;
 	private Date departure;
-	private DateFormat df;
+	private DateFormat df = SimpleDateFormat.getTimeInstance(SimpleDateFormat.SHORT, java.util.Locale.GERMANY);
 	private String url;
 	
 	private boolean delimiterSeen = false;
@@ -72,6 +62,29 @@ public class TrainEvent {
 	{
 		return url;
 	}
+
+	public void setArrival(Date arrival) {
+		this.arrival = arrival;
+	}
+	
+	public void setDestination(Station dest) {
+		destination = dest;
+	}
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	public void setUrl(String url)
+	{
+		this.url = url;
+	}
+	
+	public void setTrack(String track)
+	{
+		this.track = track;
+	}
 	
 	public boolean isParsed()
 	{
@@ -81,87 +94,10 @@ public class TrainEvent {
 	public String toString()
 	{
 		StringBuffer sb = new StringBuffer();
-		if(df != null && arrival != null) {
+		if(arrival != null) {
 			sb.append(df.format(arrival));
 		}
 		return sb.toString();
 	}
-	
-	/**
-	 * Parse html, and post a message back when/if finding the proper destination
-	 * @param html
-	 * @param handler
-	 */
-	public void parse(final String html, final Handler handler)
-	{
-		boolean hasHandled = false;
-		if(html.indexOf(" till ") != -1) {
-			hasHandled = true;
-			delimiterSeen = true;
-			String arrival = html.substring(0, html.indexOf(" "));
-			df = SimpleDateFormat.getTimeInstance(SimpleDateFormat.SHORT, java.util.Locale.GERMANY);
-			Date dd = null;
-			try {
-				dd = df.parse(arrival);
-			} catch (ParseException e) {
-				Log.i(Tag, "Exception parsing date: " + e.getMessage());
-			}
-			this.arrival = dd;
-			//Log.i(Tag, "Header for train time: " + dd);
-			
-			//also parse destination
-			String dest = html.substring(html.indexOf(" till ") + 6);
-			dest = dest.substring(0, dest.indexOf("<br>"));
-			Log.i(Tag, "Dest: " + dest);
-			
-			//Try to find it in db
-			String url = Delayed.db.getUrl(dest);
-			if(url == null) {
-				Log.w(Tag, "Could not find " + dest);
-			} else {
-				destination = new Station(dest, url);
-			}
-			
-		}
-		
-		if(html.endsWith("-<br>")) {
-			if(delimiterSeen) {
-				done = true;
-			}
-			delimiterSeen = true;
-			return;
-		}
-		
-		if(!delimiterSeen) {
-			Log.i(Tag, "Has not seen delimiter yet, ignoring: " + html);
-			return;
-		}
-		
-		if(html.startsWith("Tåg nr ")) {
-			int startNr = html.indexOf("\">") + 2;
-			String d = html.substring(startNr);
-			int endNr = d.indexOf("</a>");
-			d = d.substring(0, endNr);
-			id = Integer.valueOf(d);
-			
-			String url = html.substring(html.indexOf("href=\"") + 6);
-			final String finalUrl = url.substring(0, url.indexOf("\""));
-			this.url = finalUrl;
-			ScraperHelper.queueForParse(finalUrl, new Job<List<Nameurl>>(){
-				public void run() {
-					handler.sendMessage(Message.obtain(handler, MSG_DEST, new Object[] {finalUrl, value}));
-				}
-			});
-			return;
-		}
-		
-		if(html.startsWith("Spår ")) {
-			track = html.substring(5, html.indexOf("<br>"));
-			return;
-		}
 
-		if(!hasHandled) {
-			Log.i(Tag, "not handled: " + html);
-		}
-	}
 }

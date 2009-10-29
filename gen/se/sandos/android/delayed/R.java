582b3ef9d58aa02d2e405e911fc9df48081773c1
John Backstrand <sopues@gmail.com>
2009-10-29 08:12:30 +0100

    Remove unused row-layout

--- gen/se/sandos/android/delayed/R.java
+++ gen/se/sandos/android/delayed/R.java
@@ -20,23 +20,16 @@ public final class R {
         public static final int ProgressBar01=0x7f060006;
         public static final int StationName=0x7f060007;
         public static final int TNumber=0x7f060004;
-        public static final int TeDelayed=0x7f06000c;
-        public static final int TeDestination=0x7f060009;
-        public static final int TeExtra=0x7f06000d;
-        public static final int TeNumber=0x7f06000b;
-        public static final int TeTime=0x7f060008;
-        public static final int TeTrack=0x7f06000a;
         public static final int Time=0x7f060000;
         public static final int Track=0x7f060003;
-        public static final int WidgetText=0x7f06000e;
+        public static final int WidgetText=0x7f060008;
     }
     public static final class layout {
         public static final int eventrow=0x7f030000;
         public static final int liststations=0x7f030001;
         public static final int main=0x7f030002;
         public static final int stationrow=0x7f030003;
-        public static final int traineventrow=0x7f030004;
-        public static final int widget=0x7f030005;
+        public static final int widget=0x7f030004;
     }
     public static final class string {
         public static final int app_name=0x7f050001;

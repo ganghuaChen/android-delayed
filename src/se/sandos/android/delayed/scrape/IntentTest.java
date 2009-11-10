5be321333a38970906932a84fc2cf7339bc5ba8c
John Backstrand <sopues@gmail.com>
2009-11-10 07:52:04 +0100

    Initial support for scheduler configuration activities

--- src/se/sandos/android/delayed/scrape/IntentTest.java
+++ src/se/sandos/android/delayed/scrape/IntentTest.java
@@ -15,7 +15,9 @@ import android.util.Log;
 
 public class IntentTest
 {
-    private static final String SCHEDULER_ACTION = "se.sandos.android.delayed.Scheduler";
+    public static final String SCHEDULER_ACTION = "se.sandos.android.delayed.Scheduler";
+    public static final String SCHEDULER_SETTINGS = "se.sandos.android.delayed.SchedulerSettings";
+    
     private static final String Tag = "IntentTest";
 
     public static List<ResolveInfo> getSchedulerList(Context ctx)
@@ -27,8 +29,6 @@ public class IntentTest
         
         List<ResolveInfo> l = pm.queryBroadcastReceivers(intent, 0);
         
-        Log.v(Tag, "Got num " + l.size());
-
         return l;
     }
     
@@ -37,29 +37,23 @@ public class IntentTest
         List<ResolveInfo> l = getSchedulerList(ctx);
         
         for(ResolveInfo ri : l) {
-            Log.v(Tag, ri.toString() + " " + ri.getClass() + " " + ri.activityInfo + " " + ri.serviceInfo);
             if(ri.activityInfo != null) {
                 ActivityInfo ai = ri.activityInfo;
-                Log.v(Tag, "" + ai.name + " " + ai.processName + " " + ai.packageName);
-                Log.v(Tag, "Label: " + ctx.getResources().getString(ai.labelRes));
                 
                 try {
-                    //ComponentName cn = new ComponentName(ai.packageName, ai.name);
-                    ComponentName cn = new ComponentName(ctx, OnBoot.class);
-                    ComponentName cn2 = new ComponentName("se.sandos.android.delayed", "se.sandos.android.delayed.scrape.OnBoot");
-                    Log.v(Tag, "Cool: " + cn + " " + cn2);
+                    ComponentName cn = new ComponentName(ai.packageName, ai.name);
                     ActivityInfo aii = ctx.getPackageManager().getReceiverInfo(cn, PackageManager.GET_META_DATA);
-                    Log.v(Tag, "Got more info: " + aii.metaData.getString("Label"));
+                    if(aii.metaData != null) {
+                        Log.v(Tag, "Got more info: " + aii.metaData.getString("label"));
+                    }
                 } catch (NameNotFoundException e) {
                     Log.v(Tag, e.toString());
                 }
                 if(ai.name.indexOf("sandos") != -1) {
                     Intent in = new Intent();
                     in.setAction(SCHEDULER_ACTION);
-                    //in.setComponent(new ComponentName(ctx, OnBoot.class));
-                    //ComponentName cn = new ComponentName("se.sandos.android.delayed", "se.sandos.android.delayed.scrape.OnBoot");
-                    //ComponentName cn2 = new ComponentName(ctx, OnBoot.class);
-                    //Log.v(Tag, "Cn: " + cn + " " + cn2);
+                    in.setComponent(new ComponentName(ai.packageName, ai.name));
+
                     BroadcastReceiver br = new BroadcastReceiver() {
                         @Override
                         public void onReceive(Context context, Intent intent) {

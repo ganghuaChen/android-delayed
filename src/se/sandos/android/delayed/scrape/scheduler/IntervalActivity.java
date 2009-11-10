f0ad6701c17312b793f536cecd022c6cfc9f234c
John Backstrand <sopues@gmail.com>
2009-11-10 08:37:05 +0100

    Activity for interval scheduler

---
+++ src/se/sandos/android/delayed/scrape/scheduler/IntervalActivity.java
@@ -0,0 +1,16 @@
+package se.sandos.android.delayed.scrape.scheduler;
+
+import se.sandos.android.delayed.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IntervalActivity extends Activity {
+    
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.interval);
+    }
+}

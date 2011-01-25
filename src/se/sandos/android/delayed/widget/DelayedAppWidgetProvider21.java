0fa6d63bec5b3f789420488a77e41dda094f86c9
John B?ckstrand <John B?ckstrand@localhost>
2011-01-25 17:16:41 +0100

    Fix 1x1 widget to be semi-usable for seing tracks and cancellations

--- src/se/sandos/android/delayed/widget/DelayedAppWidgetProvider21.java
+++ src/se/sandos/android/delayed/widget/DelayedAppWidgetProvider21.java
@@ -1,6 +1,14 @@
 package se.sandos.android.delayed.widget;
 
+import se.sandos.android.delayed.R;
 
+/**
+ * 2x1 widget
+ */
 public class DelayedAppWidgetProvider21 extends DelayedAppWidgetProvider
 {
+    protected int ourLayout()
+    {
+        return R.layout.widget;
+    }    
 }
--- src/se/sandos/android/delayed/widget/DelayedAppWidgetProvider41.java
+++ src/se/sandos/android/delayed/widget/DelayedAppWidgetProvider41.java
@@ -1,6 +1,15 @@
 package se.sandos.android.delayed.widget;
 
+import se.sandos.android.delayed.R;
 
+/**
+ * 4x1 widget
+ *
+ */
 public class DelayedAppWidgetProvider41 extends DelayedAppWidgetProvider
 {
+    protected int ourLayout()
+    {
+        return R.layout.widget;
+    }
 }

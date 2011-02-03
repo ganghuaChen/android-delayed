b0c03011111fe0fbf76d4e3cba16072855f0abf3
John B?ckstrand <John B?ckstrand@localhost>
2011-02-03 08:28:17 +0100

    Try to remove old background threads if there are too many of them

--- src/se/sandos/android/delayed/scrape/ScrapePool.java
+++ src/se/sandos/android/delayed/scrape/ScrapePool.java
@@ -1,9 +1,10 @@
 package se.sandos.android.delayed.scrape;
 
-import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
+import android.util.Log;
 
 /**
  * Class for handling stuff in threads.
@@ -11,25 +12,27 @@ import java.util.concurrent.TimeUnit;
  *
  */
 public class ScrapePool {
-//	private static ExecutorService e = Executors.newFixedThreadPool(1);
-	
-	static ArrayBlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(15);
+	private static final String TAG = "ScrapePool";
+
+    static ArrayBlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(10);
 	
 	private static ThreadPoolExecutor e = new ThreadPoolExecutor(1,1, 1000, TimeUnit.MILLISECONDS, queue);
 	
 	public static void addJob(DelayRunnable r)
 	{
+	    if(e.getQueue().remainingCapacity() < 5)
+	    {
+	        for(Runnable rr : e.getQueue())
+	        {
+	            DelayRunnable dr = (DelayRunnable) rr;
+	            if(dr.getImportance().equals(DelayRunnable.Importance.NORMAL))
+	            {
+	                Log.v(TAG, "Removed task");
+	                e.remove(dr);
+	            }
+	        }
+	    }
+	    
 		e.execute(r);
 	}
-
-	
-	public static void clearOldJobs()
-	{
-        // XXX Must take real care here. Remember wake-lock releases AND service-killing is done in background threads!
-	    List<Runnable> shutdownNow = e.shutdownNow();
-	    queue = new ArrayBlockingQueue<Runnable>(5);
-	    e = new ThreadPoolExecutor(1,1, 1000, TimeUnit.MILLISECONDS, queue);
-	}
-	
-	
 }
